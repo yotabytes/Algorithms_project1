0155ff067bbc1c1ec63311cf5ce50632266a07a2
Wouter Bruyninckx <yotabytes@gmail.com>
2013-03-10 17:55:09 +0100

    Finish DoubleRatio class, experiment results have been stored in /documents on local hard drive

--- src/gna/DoubleRatio.java
+++ src/gna/DoubleRatio.java
@@ -1,5 +1,6 @@
 package gna;
 
+import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.Stopwatch;
 
 
@@ -7,13 +8,19 @@ public class DoubleRatio {
 
 	static Integer[] data;
 	static int[] permutations;
+	static String algorithm;
+	static int MAX = 2500000;
 	
 	/**
 	 * runs a double ratio experiment for insertion sort and quick sort, then outputs the results in terms of time taken and array size.
 	 * @param args[0] The algorithm to use.
 	 */
 	public static void main(String[] args) {
-		String algorithm = "insertion"; //Type of algorithm to use for sorting.
+		
+		algorithm = "quick"; //Type of algorithm to use for sorting.
+		
+		
+		
 		if (args.length == 1) { //argument given, use this as algorithm
 			if (args[0].equals("insertion") || args[0].equals("quick")) {
 				algorithm = args[0];
@@ -22,22 +29,13 @@ public class DoubleRatio {
 			}
 		}
 		
-		final int DOUBLETIMES = 10; //Times to double the array size and calculate the running time.
-		final int STARTINGSIZE = 10000; //Size of the first array of the experiment.
-		int currentsize = STARTINGSIZE;
-		
-		SortingAlgorithms Experiment = new SortingAlgorithms();
-		Stopwatch timer;
-		
-		if (algorithm.equals("insertion")) {
-			System.out.println("Insertion sort doubling ratio experiment:");
-			for (int i = 0; i < DOUBLETIMES; i++) {
-				timer = new Stopwatch();
-				data = getNewData(currentsize);	
-				Experiment.insertionSort(data);
-				System.out.print(currentsize + " " + timer.elapsedTime() + "\n");
-				currentsize *=2;
-			}
+		System.out.println("Doubling ratio experiment:");
+		double prev = timeTrial(125);
+		for (int N = 250; N <= MAX; N += N) {
+			double time = timeTrial(N);
+			StdOut.printf("%6d %7.1f ", N, time);
+			StdOut.printf("%5.1f\n", time/prev);
+			prev = time;
 		}
 		
 	}
@@ -45,10 +43,11 @@ public class DoubleRatio {
 	/**
 	 * Returns a new random permutation of integers.
 	 * @param j Size of the array.
+	 * @return Data array.
 	 */
 	private static Integer[] getNewData(int j) { //Put a new random order into the data array
 			 Integer[] generated = new Integer[j];
-			permutations = (SortingAlgorithms.getArrayOfIntegers(j));
+			permutations = (SortingAlgorithms.getRandomPermutationOfIntegers(j));
 			int k = 0;
 			for (int value : permutations) {
 			    generated[k++] = Integer.valueOf(value);
@@ -56,5 +55,29 @@ public class DoubleRatio {
 			return generated;
 	
 	}
+	
+	/**
+	 * 
+	 * @param N Size of input array to trial.
+	 * @return running time to sort the given array.
+	 */
+	public static double timeTrial(int N) {
 
+		SortingAlgorithms Experiment = new SortingAlgorithms();
+		Stopwatch timer;
+		
+		if (algorithm.equals("insertion")) {
+				timer = new Stopwatch();
+				data = getNewData(N);	
+				Experiment.insertionSort(data);
+				return timer.elapsedTime();
+
+		} else {
+				timer = new Stopwatch();
+				data = getNewData(N);	
+				Experiment.quickSort(data);
+				return timer.elapsedTime();
+		}
+		
+	}
 }
